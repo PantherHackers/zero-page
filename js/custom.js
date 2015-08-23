fda0926797f64a3dcdc3ed2f230c07fa40bf2851
luis <ferrerluis97@gmail.com>
2015-08-23 19:54:36 -0400

    Finished finished

--- js/custom.js
+++ js/custom.js
@@ -53,52 +53,62 @@ myApp.controller('mainController', ['$scope', '$http', '$sce', function($scope, 
 	$scope.result = '';
 	$scope.experience = 'none';
 	$scope.signUp = true;
+	$scope.buttonStatus = true;
+	
+	
+	$scope.isDisabled = function () {
+		
+		return !$scope.buttonStatus;
+	};
 	
 	$scope.submit = function() {
 		
 		if ($scope.name.length !== 0 && $scope.campusId.length !== 0) {
 			
-			$scope.resultError = '';
-			$scope.nameError = '';
-			$scope.campusIdError = '';
-			
-			var info = {
+			if ($scope.buttonStatus) {
 				
-				"name": $scope.name,
-				"campusId": $scope.campusId,
-				"experience": $scope.experience,
-				"signUp": $scope.signUp
-			};
-			
-			var script_url = "https://script.google.com/macros/s/AKfycbxlDE99VBlCgPxYlGjlA2Qow5RZST9MYn0eyMoyL1lEcj7dgcM/exec";
-			$.ajax({
-				method:'POST',
-				url: script_url,
-				data: info
-			})
+				$scope.buttonStatus = false;
+				$scope.resultError = '';
+				$scope.nameError = '';
+				$scope.campusIdError = '';
+				
+				var info = {
+					
+					"name": $scope.name,
+					"campusId": $scope.campusId,
+					"experience": $scope.experience,
+					"signUp": $scope.signUp
+				};
+				
+				var script_url = "https://script.google.com/macros/s/AKfycbxlDE99VBlCgPxYlGjlA2Qow5RZST9MYn0eyMoyL1lEcj7dgcM/exec";
+				$.ajax({
+					method:'POST',
+					url: script_url,
+					data: info
+				})
 				.done(function(){
 					$scope.$apply(function() {
 						
+						$scope.buttonStatus = true;					
 						$scope.result = $sce.trustAsHtml('<br>Thanks for RSVPing. We look forward to meeting you!');
 					});
 				})
 				.fail(function() {
 					$scope.$apply(function() {
 						
+						$scope.buttonStatus = true;					
 						$scope.resultError = 'my-alert';
 						$scope.result = $sce.trustAsHtml('<br>An error has ocurred. Please refresh the website and try again or contact <a href="mailto:faq@zero.pantherhackers.com">help@pantherhackers.com</a>');
 					});
 				});
-			
-			
-			
+			}
+				
 		} else {
 			
 			$scope.resultError = 'my-alert';
 			$scope.result = $sce.trustAsHtml('<br>Please fill in all fields.');
 			$scope.nameError = 'my-error';
 			$scope.campusIdError = 'my-error';
-			
 		}
 	}
 }]);
